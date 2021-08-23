64d4c2e1d1cb6033fbec81a98b52524dca079a7d
Sean <reeegan2472@gmail.com>
2021-08-23 16:34:21 +0900

    Javascript update add consol.log x 3

--- _HelloJavaScript.js
+++ _HelloJavaScript.js
@@ -4,3 +4,7 @@ console.debug("Hellow Javascript");
 console.group("MyGroup");
 console.debug("안녕하세요.");
 console.debug("반갑습니다.");
+
+console.debug("수정1.");
+console.debug("수정2.");
+console.debug("수정3.");
